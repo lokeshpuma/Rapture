e55ddbf1fe78b1f3c08eb244844640cef439ddc2
lokesh <lokeshpuma29@gmail.com>
2025-09-15 21:02:22 +0530

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,22 @@
 import express from "express";
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 
 connectDB();
 
 app.get("/",(req,res)=>
     res.send("<h1>hello from server</h1>"))
+
+app.use("/api/users",userRoutes)
 
 const startServer = async()=>{
     try {
