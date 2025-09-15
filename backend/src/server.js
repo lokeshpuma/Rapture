b4127cc504e86dadd251c9f825554e9aa58cd792
lokesh <lokeshpuma29@gmail.com>
2025-09-15 18:27:34 +0530

    start serverfunction added

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,16 @@ connectDB();
 app.get("/",(req,res)=>
     res.send("<h1>hello from server</h1>"))
 
+const startServer = async()=>{
+    try {
+        await connectDB();
+        app.listen(ENV.PORT,()=>
+            console.log("server is running on port :",ENV.PORT));
+    } catch (error) {
+        console.error("error in starting server :",error.message);
+        process.exit(1);
+    }
+};
 
-app.listen(ENV.PORT,()=>
-    console.log("server is running on port :",ENV.PORT));
+
+startServer();
