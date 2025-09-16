e7255884d92eaf339498d1df0c4468b65eed9b1e
lokesh <lokeshpuma29@gmail.com>
2025-09-16 10:54:34 +0530

    Fix import issues and rename misspelled route file
    - Change User imports from default to named imports in controllers
    - Rename coment.route.js to comment.route.js to resolve module not found error

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler";
 import { getAuth } from "@clerk/express";
 import Notification from "../models/notification.model.js";
-import User from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 
 export const getNotifications = asyncHandler(async (req, res) => {
   const { userId } = getAuth(req);
